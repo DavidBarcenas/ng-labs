569d526111af3016d08aa310227593a7c15558a3
David Bárcenas <dbarcenas.dev@gmail.com>
2024-05-06 22:15:42 -0600

    Agrega componente auth-shell y marcado basico

---
+++ src/app/auth/auth-shell/auth-shell.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AuthShellComponent } from './auth-shell.component';
+
+describe('AuthShellComponent', () => {
+  let component: AuthShellComponent;
+  let fixture: ComponentFixture<AuthShellComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AuthShellComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(AuthShellComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/auth/auth-shell/auth-shell.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-shell',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-shell.component.html',
+  styleUrl: './auth-shell.component.scss'
+})
+export class AuthShellComponent {
+
+}
