6e1eef8c6390450ec4130923133db1e25e55c0d8
David Bárcenas <dbarcenas.dev@gmail.com>
2024-05-08 16:27:58 -0600

    Inicia la maquetacion base del login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthShellComponent } from "./auth/auth-shell/auth-shell.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, AuthShellComponent]
 })
 export class AppComponent {}
