e5a56ad5435d79afd4ef443a7978c39400e8fb4b
David Bárcenas <dbarcenas.dev@gmail.com>
2024-05-16 22:43:10 -0600

    Crea la estructura del formulario de login

--- src/app/auth/auth-login/auth-login.component.ts
+++ src/app/auth/auth-login/auth-login.component.ts
@@ -5,7 +5,6 @@ import { AuthShellComponent } from '../auth-shell/auth-shell.component';
   selector: 'app-auth-login',
   standalone: true,
   templateUrl: './auth-login.component.html',
-  styleUrl: './auth-login.component.scss',
   imports: [AuthShellComponent],
 })
 export class AuthLoginComponent {}
