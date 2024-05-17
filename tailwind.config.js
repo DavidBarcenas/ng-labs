a7431f2c0346c23f8896513437feb01c5fde8628
David Bárcenas <dbarcenas.dev@gmail.com>
2024-05-16 21:38:42 -0600

    Personaliza paleta de colores y fuente

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Manrope', 'sans-serif'],
+      },
+      colors: {
+        gray: {
+          100: '#f7fafc',
+          200: '#edf2f7',
+          300: '#e2e8f0',
+          400: '#cbd5e0',
+          600: '#718096',
+        }
+      }
+    },
   },
   plugins: [],
-}
+};
