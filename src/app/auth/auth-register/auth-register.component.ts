dd6136bf7f1fd16b2abd4c14b49b5684411e42dc
David Bárcenas <dbarcenas.dev@gmail.com>
2024-05-25 00:06:58 -0600

    Crea estructura de la pagina de registro

--- src/app/auth/auth-register/auth-register.component.ts
+++ src/app/auth/auth-register/auth-register.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { AuthShellComponent } from "../auth-shell/auth-shell.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-auth-register',
-  standalone: true,
-  imports: [],
-  templateUrl: './auth-register.component.html',
-  styleUrl: './auth-register.component.scss'
+    selector: 'app-auth-register',
+    standalone: true,
+    templateUrl: './auth-register.component.html',
+    imports: [AuthShellComponent, RouterLink]
 })
 export class AuthRegisterComponent {
+  showPass = signal(false);
 
+  togglePass() {
+    this.showPass.update((value) => !value);
+  }
 }
