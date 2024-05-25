138ae1ec0b0b268fb78b91ad03620ab3c5ab176f
David Bárcenas <dbarcenas.dev@gmail.com>
2024-05-24 23:57:07 -0600

    Agrega propiedades de entrada para registro

--- src/app/auth/auth-shell/auth-shell.component.ts
+++ src/app/auth/auth-shell/auth-shell.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-auth-shell',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './auth-shell.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AuthShellComponent {}
+export class AuthShellComponent {
+  isRegister = input(false, {
+    transform: (value: boolean | string) => (typeof value === 'string' ? value === '' : value),
+  });
+  sectionTitle = computed(() => (this.isRegister() ? 'Crea una cuenta' : 'Inicia sesión'));
+  socialBtnText = computed(() => (this.isRegister() ? 'Registrarse' : 'Ingresar'));
+}
