017b836fbcadcd2240d43a6a957b369b88405d35
David Bárcenas <dbarcenas.dev@gmail.com>
2024-06-10 21:24:15 -0600

    Agregar pagina para restablecer contrasena

--- src/app/auth/auth-help/auth-help.component.ts
+++ src/app/auth/auth-help/auth-help.component.ts
@@ -1,9 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-auth-help',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, NgOptimizedImage],
   templateUrl: './auth-help.component.html',
 })
 export class AuthHelpComponent {
